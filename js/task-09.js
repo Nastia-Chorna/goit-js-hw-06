3819d22029f57dc3af888369f277454402885460
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-17 18:35:50 +0300

    8 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+
+const changeColorEl = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+changeColorEl.addEventListener('click', changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeColor () {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  span.textContent = color;
 }
+
