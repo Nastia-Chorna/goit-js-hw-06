b5230407068e7d5bb75af749f3ec484cd3c10b4c
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-17 18:33:33 +0300

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
 
+const form = document.querySelector(".login-form");
+
+const onForSubmit = event => {
+    event.preventDefault()
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if (email === '' || password === '') {
+        return alert("всі поля повинні бути заповнені");
+    }
+    const formData = {email, password};
+    console.log(formData);
+    event.currentTarget.reset();
+}
+form.addEventListener('submit', onForSubmit);
+
