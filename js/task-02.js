a7911a29df7b3f4ff6f49cb4d2617d2b3cc38985
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-17 18:15:20 +0300

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const itemEl = document.querySelector('#ingredients');
+
+const listEl = ingredients.forEach((ingredient) => {
+
+const itemListEl1 = document.createElement('li')
+itemListEl1.classList.add('item')
+itemListEl1.textContent = ingredient
+
+itemEl.appendChild(itemListEl1);
+
+})
+console.log(itemEl);
+
