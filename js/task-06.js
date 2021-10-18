7784c46e44f420cf481447d66bfb5cf7812bceb2
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-18 22:54:59 +0300

    6 task changed

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,14 @@
 
 let inputValid = document.getElementById("validation-input");
 
-let length = inputValid.getAttribute("data-length");
-let inLength = parseInt(length, 6);
-
-inputValid.onInput = function () {
-    if (inputValid.value.length === inLength) {
-        inputValid.classList.add("valid");
-        inputValid.classList.remove("invalid");
+const sumpolInput = (event) => {
+    if (event.currentTarget.value.length === 6) {
+        event.currentTarget.classList.add("valid");
+        event.currentTarget.classList.replace("valid", "invalid");
     }
-    if (inputValid.value.length === 0) {
-        inputValid.classList.remove("valid");
-        inputValid.classList.remove("invalid");
-    }
-    if (inputValid.value.length !== inLength && inputValid.value.length !== 0) {
-        inputValid.classList.add("invalid")
+    else {
+        event.currentTarget.classList.add("invalid");
+        event.currentTarget.classList.replace("valid", "invalid");
     }
 };
+inputValid.addEventListener("blur", sumpolInput);
