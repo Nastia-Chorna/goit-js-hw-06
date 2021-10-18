f4321427e9d0fef9345f688b17315a27597b4818
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-10-18 22:24:16 +0300

    5 task change

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,12 @@
 
-let input = document.getElementById('name-input');
-let output = document.getElementById('name-output');
+let inputEl = document.querySelector('#name-input');
+let outputEl = document.querySelector('#name-output');
 
-input.oninput = function () {
-    if (input.value === '') {
-       output.innerHTML = 'Anonymous';
-    } else{
-    output.innerHTML = input.value;
+inputEl.addEventListener('input', onInput);
+function onInput(event) {
+    outputEl.textContent = event.currentTarget.value.trim();
+    if (outputEl.textContent === '') {
+        outputEl.textContent = "Anonymous";
     }
 }
+
